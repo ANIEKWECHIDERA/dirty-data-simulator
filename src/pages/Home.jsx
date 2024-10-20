2834311a8d0c11bcf71ae9c9c10d1bef9fadead0
ANIEKWECHIDERA <c.hideraaniekwederah73@gmail.com>
2024-10-20 14:45:42 +0100

    ADded extra components and installed tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Hero from "../components/Hero";
+import NavBar from "../components/NavBar";
+import About from "../components/About";
+import Features from "../components/Features";
+import HowItWorks from "../components/HowItWorks";
+import Contact from "../components/Contact";
+import CTA from "../components/CTA";
+import FAQ from "../components/FAQ";
+import Footer from "../components/Footer";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <HowItWorks />
+      <CTA />
+      <FAQ />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
